c2f8a5986a7ac831b8ea91c90eaa2f18d3a7594e
takegami <hiroyuki.takegami@groove-x.com>
2019-04-10 22:10:49 +0900

    Add vim config

---
+++ vim/template/template.cpp
@@ -0,0 +1,12 @@
+/**
+ * <+FILENAME+>
+ * Created: <+DATE+>
+ *
+ */
+
+#include "<+FILENAME+>.hpp"
+
+int main(int argc, char** argv) {
+
+  return 0;
+}
---
+++ vim/template/template.h
@@ -0,0 +1,20 @@
+/**
+ * <+FILENAME+>
+ * Created: <+DATE+>
+ *
+ */
+
+#ifndef __<+FILENAME_UPPER+>_H__
+#define __<+FILENAME_UPPER+>_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+<+CURSOR+>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __<+FILENAME_UPPER+>_H__
